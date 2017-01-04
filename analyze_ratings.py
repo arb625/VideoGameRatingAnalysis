07ccd45ecb32eaecaf128a100925208af082444a
Anurag Baddam <baddamanu@berkeley.edu>
2017-01-03 22:28:58 -0500

    added user score vs global sales regression

--- analyze_ratings.py
+++ analyze_ratings.py
@@ -4,37 +4,44 @@ import pandas as pd
 import scipy.interpolate as spi
 import matplotlib.pyplot as plt
 
-def is_score_valid(score):
+def is_number(string):
     try:
-        float(score)
+        float(string)
         return True
     except ValueError:
         return False
 
 users_scores = []
 critic_scores = []
+global_sales_values = []
 with open('Video_Games_Sales_as_at_22_Dec_2016.csv', 'r') as csvfile:
     row_reader = csv.DictReader(csvfile)
     for row in row_reader:
         user_score = row["User_Score"]
         critic_score = row["Critic_Score"]
+        global_sales_value = row["Global_Sales"] 
 
-        if is_score_valid(user_score) and is_score_valid(critic_score):  #make sure both scores are numbers
+        if is_number(user_score) and is_number(critic_score) and is_number(global_sales_value):  #make sure both scores are numbers
             users_scores.append(float(user_score))
             critic_scores.append(float(critic_score))
+            global_sales_values.append(float(global_sales_value))
             
-linear_rel = np.polyfit(users_scores, critic_scores, 1)  #deg = 1: least squares linear regression
-slope = linear_rel[0]
-critic_intercept = linear_rel[1]
+users_vs_critics_scores = np.polyfit(users_scores, critic_scores, 1)  #deg = 1: least squares linear regression
 
-print("slope:", slope)    #slope and y-intercept
-print("critic_intercept:", critic_intercept, "\n")
-
+plt.subplot(2,1,1)
 plt.title("User Scores vs Critic Scores for Video Games")
-plt.xlabel("User Score")
 plt.ylabel("Critic Score")
 plt.plot(users_scores, critic_scores, 'o')  #plot actual data points
-plt.plot(users_scores, np.polyval(linear_rel, users_scores), 'r')  #plot line of best fit
+plt.plot(users_scores, np.polyval(users_vs_critics_scores, users_scores), 'r')  #plot line of best fit
+
+users_scores_vs_global_sales = np.polyfit(users_scores, global_sales_values, 1)  #deg = 1: least squares linear regression
+
+plt.subplot(2,1,2)
+plt.title("User Scores vs Global Sales for Video Games")
+plt.xlabel("User Score")
+plt.ylabel("Global Sales")
+plt.plot(users_scores, global_sales_values, 'g+')  #plot actual data points
+plt.plot(users_scores, np.polyval(users_scores_vs_global_sales, users_scores), 'r')  #plot line of best fit
 
 plt.show()
 
