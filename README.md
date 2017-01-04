94f331e5b243c00e6b2bbc2e0c5ec59b124d6961
Anurag Baddam <baddamanu@berkeley.edu>
2017-01-03 19:10:29 -0500

    added pandas mean analysis

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # VideoGameRatingAnalysis
-A script that plots the user scores vs. critic scores for the Metacritic video game ratings obtained from this [dataset](https://www.kaggle.com/rush4ratio/video-game-sales-with-ratings) and performs linear regression to observe the relationship between these scores.
+A script that plots the user scores vs. critic scores for the Metacritic video game ratings obtained from this [dataset](https://www.kaggle.com/rush4ratio/video-game-sales-with-ratings) and performs linear regression to observe the relationship between these scores. Also performs mean analysis on the ratings grouped on various features of the games such as genre and platofrm.
 
-Uses [numpy](http://www.numpy.org/), [scipy](https://www.scipy.org/), and [matplotlib](http://matplotlib.org/).
+Uses [numpy](http://www.numpy.org/), [scipy](https://www.scipy.org/), [matplotlib](http://matplotlib.org/), and [pandas](http://pandas.pydata.org/).
 
 ![](/user_vs_critic_scores.png)
--- analyze_ratings.py
+++ analyze_ratings.py
@@ -1,5 +1,6 @@
 import csv
 import numpy as np
+import pandas as pd
 import scipy.interpolate as spi
 import matplotlib.pyplot as plt
 
@@ -22,17 +23,37 @@ with open('Video_Games_Sales_as_at_22_Dec_2016.csv', 'r') as csvfile:
             users_scores.append(float(user_score))
             critic_scores.append(float(critic_score))
             
-linear_rel = np.polyfit(users_scores, critic_scores, 1)  #deg = 1: linear regression
+linear_rel = np.polyfit(users_scores, critic_scores, 1)  #deg = 1: least squares linear regression
 slope = linear_rel[0]
 critic_intercept = linear_rel[1]
 
 print("slope:", slope)    #slope and y-intercept
-print("critic_intercept:", critic_intercept)
+print("critic_intercept:", critic_intercept, "\n")
 
 plt.title("User Scores vs Critic Scores for Video Games")
 plt.xlabel("User Score")
 plt.ylabel("Critic Score")
-plt.plot(users_scores, critic_scores, 'o')  #actual data points
-plt.plot(users_scores, np.polyval(linear_rel, users_scores), 'r')  #linear regression/line of best fit
+plt.plot(users_scores, critic_scores, 'o')  #plot actual data points
+plt.plot(users_scores, np.polyval(linear_rel, users_scores), 'r')  #plot line of best fit
 
 plt.show()
+
+df = pd.read_csv('Video_Games_Sales_as_at_22_Dec_2016.csv')
+
+def mean_of_col(df, col_name):
+    return pd.to_numeric(df[col_name], errors='coerce').mean()
+
+print("Average User Score:", mean_of_col(df, "User_Score"))
+print("Average Critic Score:", mean_of_col(df, "Critic_Score"), "\n")
+
+print("Average Critic Scores by Genre")
+print(df.groupby("Genre").mean()["Critic_Score"].sort_values(ascending=False))
+print()
+
+print("Average Critic Scores by Platform")
+print(df.groupby("Platform").mean()["Critic_Score"].sort_values(ascending=False))
+print()
+
+print("Average Critic Scores by Publisher")
+print(df.groupby("Publisher").mean()["Critic_Score"].sort_values(ascending=False))
+print()
